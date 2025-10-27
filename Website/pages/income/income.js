0c31f1b2c4f1ec95313313477e119bfe6320e891
oofiet <97848841+oofiet@users.noreply.github.com>
2025-10-27 15:02:12 -0600

    Started implementing income
    Added some content to the income page, have yet to add functionality

---
+++ Website/pages/income/income.js
@@ -0,0 +1,60 @@
+/**
+ * @author Roy Rodriguez
+ * @license Zlib
+ * @since 2025-10-27
+ * @description Creates, modifies, removes, and displays income
+ * from user data.
+ */
+
+function loadIncome()
+{
+
+}
+
+function openAddIncome()
+{
+
+}
+
+function closeAddIncome()
+{
+
+}
+
+function addIncome()
+{
+
+}
+
+function compareIncomes()
+{
+
+}
+
+function modifyIncome()
+{
+
+}
+
+function modifyIncomes()
+{
+
+}
+
+function deleteIncome()
+{
+
+}
+
+function deleteIncomes()
+{
+    
+}
+
+async function main()
+{
+    await account.loadFromStorage();
+
+    const addButton = document.querySelector('#add-button');
+    const modifyButton = document.querySelector('#modify-button');
+}
