8520c4d7498f1c5ada1e42507e108a23cfb902e6
Joshua White <140011936+mugiwara-joshuawhite@users.noreply.github.com>
2025-11-11 14:37:28 -0700

    First HomePage Coding Session: Income now shows all income streams
    More changes need to be made, specifically:
    - Showing recurrance data
    -formatting dates better
    - Only showing Amount and Next Payment until income-category button is pressed, where it will then show the rest of the data

--- Website/pages/home/home.js
+++ Website/pages/home/home.js
@@ -1,5 +1,60 @@
+/**
+ * fills the income section with the streams from user account
+ */
+function loadIncome() {
+
+    //Values
+    const streams = account.streams;    //Data to show
+    const incomeList = document.querySelector(".income-list");  //Where to show the data
+
+    //Loop through streams
+    for (let i = 0; i < streams.length; i++) {
+
+        //Main parts of a stream, the name and the data
+        let category = document.createElement('button');
+        let list = document.createElement('ul');
+
+        let items = [document.createElement('li'),  //Amount
+            document.createElement('li'),           //Next due date
+            document.createElement('li'),           //End date
+            //To add later - recurrance data
+        ];
+        //Data holds the actual values of each item
+        let data = [document.createElement('span'),
+            document.createElement('span'),
+            document.createElement('span'),
+        ];
+
+        //Fill out category
+        category.innerHTML = streams[i].text;
+        category.classList.add("finance-category");
+        //TODO: add event listener to expand stream on category
+
+        items[0].innerHTML = "Amount: $";
+        items[1].innerHTML = "Next Payday: ";
+        items[2].innerHTML = "Goes until: ";
+        
+        //TODO: Format dates better
+        data[0].innerHTML = streams[i].amount;
+        data[1].innerHTML = streams[i].date;
+        data[2].innerHTML = streams[i].endDate;
+        
+        //If there were a way to know how many values an object had i would do this differently
+        for (let j = 0; j < 3; j++) {
+            data[j].classList.add('income-text');
+            items[j].appendChild(data[j]);
+            list.appendChild(items[j]);
+        }
+
+        incomeList.appendChild(category);
+        incomeList.appendChild(list);
+    }
+}
+
 async function main() {
     await account.loadFromStorage();
+
+    loadIncome();
 }
 
 main();
